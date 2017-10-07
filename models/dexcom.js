90f91261830a2751debc72479a9d21de5ab50f73
John Barson <jkbarson@gmail.com>
2017-10-07 19:51:09 -0400

    change schema for dexcom.  see if way to reduce info from API call

--- models/dexcom.js
+++ models/dexcom.js
@@ -6,7 +6,10 @@ const mongoose = require('mongoose');
 const Schema = moongoose.Schema;
 
 const dexcomSchema = new Schema({
-  egv: {type: [
+  unit : { type: String },
+  rateUnite: { type: String },
+  egv: { type: [
+    systemTime: { type: DateTime, required: true }, // find way to limit what is pulled in API request to reduce types for mongoose
     displayTime: { type: DateTime, required: true },  //DateTime is not a supported type. How to solve?
     value: { type: Number, required: true },  //Will Decimals work with Mongoose?
     status: { type: String, required: true },

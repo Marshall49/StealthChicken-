9aaf84df6c7939d8aca357d200bf26ca663fb3c0
Jess <jsscthomp@gmail.com>
2017-10-07 14:13:49 -0400

    setting up home and profile page

---
+++ Main/client/src/pages/Profile.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import Footer from '../../components/Footer';
+import Nav from '../../components/Nav';
+import { Article, ArticleList } from '../../components/ArticleFeed';
+import CommentFeed from '../../components/CommentFeed';

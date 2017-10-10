9f3007eed776e5346369d1e71bc1b6a9c1dbb473
John Barson <jkbarson@gmail.com>
2017-10-10 16:04:01 -0400

    fix decom schema

--- models/dexcom.js
+++ models/dexcom.js
@@ -3,19 +3,19 @@ module.exports = {
 };
 
 const mongoose = require('mongoose');
-const Schema = moongoose.Schema;
+const Schema = mongoose.Schema;
 
 const dexcomSchema = new Schema({
   unit : { type: String },
   rateUnite: { type: String },
-  egv: { type: [
-    systemTime: { type: DateTime, required: true }, // find way to limit what is pulled in API request to reduce types for mongoose
-    displayTime: { type: DateTime, required: true },  //DateTime is not a supported type. How to solve?
+  egv: { type: [{
+    systemTime: { type: Date, required: true }, // find way to limit what is pulled in API request to reduce types for mongoose
+    displayTime: { type: Date, required: true },  //DateTime is not a supported type. How to solve?
     value: { type: Number, required: true },  //Will Decimals work with Mongoose?
     status: { type: String, required: true },
     trend: { type: String, required: true },
     trendRate: { type: Number, required: true }
-  ]}
+  }]}
 })
 
 

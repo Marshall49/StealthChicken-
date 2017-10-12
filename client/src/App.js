e86238531a58b794ea9142887bf97bde7a814ae1
Jess <jsscthomp@gmail.com>
2017-10-12 18:40:10 -0400

    made routes in App.js and started on routes in API

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>BRUH</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/physician" component={LogInPage} />
+        <Route exact path="/physician/:id" component={Profile} />
+        <Route exact path="/physician/:???????" component={Dashboard} />
+        <Route exact path="/about" component={About} />
+        {/* redirect to acct creation */}
+        <Route component={} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
+

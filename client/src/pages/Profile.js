2638d9bb59c1aa7f1e72532cf7aa54712323a8f2
Caitlin Rose <cnrose02@gmail.com>
2017-10-13 18:46:44 -0400

    error fixing

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Footer from '../../components/Footer';
-import Nav from '../../components/Nav';
-import { Article, ArticleList } from '../../components/ArticleFeed';
-import CommentFeed from '../../components/CommentFeed';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+import { Article, ArticleList } from '../components/ArticleList';
+import CommentFeed from '../components/CommentFeed';
 
 export default class Profile extends Component {
 

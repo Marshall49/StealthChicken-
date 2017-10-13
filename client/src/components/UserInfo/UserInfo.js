0dcfdc70cd54ee34cba2108f1581e0b8f2c3c4d0
Jess <jsscthomp@gmail.com>
2017-10-13 18:21:01 -0400

    took out imgURL for userinfo for now

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -4,7 +4,7 @@ const UserInfo = () =>
     <div className='userInfo'>
         <h2> {this.props.name} </h2>
         <h4> {this.props.date} </h4>
-        <img src={this.props.imgURL} />
+        {/* <img src={this.props.imgURL} /> */}
     </div>;
 
 export default UserInfo;

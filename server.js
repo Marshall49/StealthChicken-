7f2f39963018fb8abd070d3f16c05e36ca562116
John Barson <jkbarson@gmail.com>
2017-10-14 14:09:39 -0400

    update server.js, move auth code to API/index.js

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const physician = require("./models/physician.js");
 const dCase = require("./models/dCase.js");
 const dexcom = require("./models/dexcom.js");
 // Oauth2 library
-const clientOAuth2 = require('client-oauth2');
+const ClientOAuth2 = require('client-oauth2');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 //Here are the Heroku deploy "Mlab" Mongo URI for the Dexcom Client Secret and the mongo lab
-const CLIENTSECRET = process.env.DEXCOM_CLIENT_SECRET || "";
+const CLIENT_SECRET = process.env.DEXCOM_CLIENT_SECRET || "";
 const MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://localhost/Stealth_Chicken'
+const REDIRECT_URL = process.env.REDIRECT_URL || ''
+const CLIENT_ID = process.env.DEXCOM_CLIENT_ID || ''
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -64,91 +66,6 @@ exampleUser.save(function(error, doc) {
   }
 });
 
-
-//=================Patient Data Authentication and Authorization==============================================
-const pAuth = new ClientOAuth2({
-  clientId: 'KGkhhNwb8IkWa9WYH9ibHfLTONzAAdGr',
-  clientSecret: CLIENTSECRET, //use environmental variable
-  accessTokenUri: 'https://sandbox-api.dexcom.com', // https://api.dexcom.com/v1/oauth2/token is used for non sandox
-  authorizationUri: 'https://sandbox-api.dexcom.com',
-  redirectUri: 'http://example.com/auth/dexcom/callback', //Need to make a redirectUri
-  scopes: ['offline_access']
-});
-
-// Get the authorization code, token, and
-app.get('/auth/dexcom', function (req, res) {
-  var uri = pAuth.code.getUri()
-
-  res.redirect(uri)
-})
-
-app.get('/auth/dexcom/callback', function (req, res) {
-  pAuth.code.getToken(req.originalUrl)
-    .then(function (user) {
-      console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... }
-
-      // Refresh the current users access token.
-      user.refresh().then(function (updatedUser) {
-        console.log(updatedUser !== user) //=> true
-        console.log(updatedUser.accessToken)
-      })
-    // API request
-      let options = {
-        "method": "GET",
-        "hostname": "sandbox-api.dexcom.com",
-        "port": null,
-        "path": "/v1/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-07-16T15:45:00", //testing with just one month data to show concept.  Future: ability to select dates, then later make API calls using user token
-        "headers": {
-          "authorization": "Bearer" + user.accessToken,
-          }
-      };
-
-      // var req = http.request(options, function (res) {
-      //   var chunks = [];
-      //
-      //   res.on("data", function (chunk) {
-      //     chunks.push(chunk);
-      //   });
-      //
-      //   res.on("end", function () {
-      //     var body = Buffer.concat(chunks);
-      //     console.log(body.toString());
-      //   });
-      // });
-
-      // make API call with http request
-      var req = http.request(options, function (res) {
-        // create an empty object to store the result
-        var result = {};
-        // on receipt of the data, push it into "result"
-        res.on("data", function (chunk) {
-          result.push(chunk);
-        });
-        // fit the result data into the dexcom schema
-        let newPat = new dexcom(result);
-        // Save the data to the db
-        newPat.save(function(err, doc) {
-          if (err) {
-            console.log(err);
-          }
-          else {
-            console.log(doc);
-          }
-        });
-
-        res.on("end", function () {
-          var body = Buffer.concat(chunks);
-          console.log(body.toString());
-        });
-      });
-
-      req.end();
-
-      // Should we store the token into a database?  Future feature allowing API calls from the app using stored tokens?
-      // return res.send(user.accessToken)
-    })
-})
-// ==========================End Patient Data Authentication and Authorization ===================================
 
 // Start the API server
 app.listen(PORT, function() {

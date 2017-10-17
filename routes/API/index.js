02e71177b68a87edea5edb21e728bb965ca41a9f
John Barson <jkbarson@gmail.com>
2017-10-17 14:55:27 -0400

    update

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const physicianRoutes = require("./physician");
-const ClientOAuth2 = require('client-oauth2');
-const dCase = require("../../models/dexcom.js");
+
 
 // =============Patient Authentication=================================
 const CLIENT_SECRET = process.env.DEXCOM_CLIENT_SECRET || "";
 const REDIRECT_URL = process.env.REDIRECT_URL || ''
 const CLIENT_ID = process.env.DEXCOM_CLIENT_ID || ''
+const ClientOAuth2 = require('client-oauth2');
+const dexcom = require("../../models/dexcom.js");
+const https = require('https');
 
 const pAuth = new ClientOAuth2({
   clientId: 'EOoo2Rk7zVA7oAA7kpFn9vDGwHzZ39u7',
@@ -37,34 +39,56 @@ router.get('/auth/dexcom/callback', function (req, res) {
       })
 
       // Sign API requests on behalf of the current user.
-      user.sign({
-        method: 'get',
-        url: 'http://example.com'
-      })
+      // user.sign({
+      //   method: 'get',
+      //   url: 'https://sandbox-api.dexcom.com/v1/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-06-17T15:45:00'
+      // })
+      // return res.send(user.accessToken)
 
-      // We should store the token into a database.
-      return res.send(user.accessToken)
-    })
-})
-//   pAuth.code.getToken(req.originalUrl)
-//     .then(function (user) {
-//       console.log(user); //=> { accessToken: '...', tokenType: 'bearer', ... }
-//       console.log("IM OVER HERE!!!");
-//       // Refresh the current users access token.
-//       user.refresh().then(function (updatedUser) {
-//         console.log(updatedUser !== user); //=> true
-//         console.log(updatedUser.accessToken);
-//       });
-//     // API request
-//       let options = {
-//         "method": "GET",
-//         "hostname": "sandbox-api.dexcom.com",
-//         "port": null,
-//         "path": "/v1/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-07-16T15:45:00", //testing with just one month data to show concept.  Future: ability to select dates, then later make API calls using user token
-//         "headers": {
-//           "authorization": "Bearer " + user.accessToken,
-//           }
-//       };
+      // API request
+        var options = {
+          "method": "GET",
+          "hostname": "sandbox-api.dexcom.com",
+          "port": null,
+          "path": "/v1/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-06-17T15:45:00", //testing with just one month data to show concept.  Future: ability to select dates, then later make API calls using user token
+          "headers": {
+            "authorization": "Bearer " + user.accessToken,
+            }
+        };
+
+        console.log(options.headers.authorization);
+        // make API call with http request
+        https.request(options, function (res) {
+          if (err) throw (err);
+          // create an empty object to store the result
+          var potato = [];
+          // on receipt of the data, push it into "result"
+          res.on("data", function (chunk) {
+            potato.push(chunk);
+          });
+          console.log(potato);
+
+          res.on("end", function () {
+            var body = Buffer.concat(potato);
+            console.log(body.toString());
+          });
+          // fit the result data into the dexcom schema
+          // let newPat = new dexcom(result);
+          // // Save the data to the db
+          // newPat.save(function(err, doc) {
+          //   if (err) {
+          //     console.log(err);
+          //   }
+          //   else {
+          //     console.log(doc);
+          //   }
+          // });
+      // // We should store the token into a database.
+      //   return res.send(user.accessToken)
+    });
+  });
+});
+
 //
 //       // var req = http.request(options, function (res) {
 //       //   var chunks = [];
@@ -79,25 +103,7 @@ router.get('/auth/dexcom/callback', function (req, res) {
 //       //   });
 //       // });
 //
-//       // make API call with http request
-//       var req = http.request(options, function (res) {
-//         // create an empty object to store the result
-//         var result = {};
-//         // on receipt of the data, push it into "result"
-//         res.on("data", function (chunk) {
-//           result.push(chunk);
-//         });
-//         // fit the result data into the dexcom schema
-//         let newPat = new dexcom(result);
-//         // Save the data to the db
-//         newPat.save(function(err, doc) {
-//           if (err) {
-//             console.log(err);
-//           }
-//           else {
-//             console.log(doc);
-//           }
-//         });
+
 //
 //         res.on("end", function () {
 //           var body = Buffer.concat(chunks);

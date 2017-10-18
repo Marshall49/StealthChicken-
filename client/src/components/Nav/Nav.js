626f83c07fcb81e0e72749a1af792d0198fcf889
Jess <jsscthomp@gmail.com>
2017-10-18 16:32:34 -0400

    more navbar changes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,28 +2,33 @@ import React from 'react';
 import "./Nav.css";
 
 const Nav = () =>
+    <header>
+        <div className="blur"></div>
+        <div className="header_bar">
+            <nav className="navbar navbar-dark bg-dark">
+                
+                <div className="container-fluid">
+                    <div className="navbar-header shift">
+                        <a className="navbar-brand" href="/Dashboard"><img src="https://vectr.com/champageonponce/aI2dP1Qsu.svg?width=273&height=275&select=aI2dP1Qsupage0" alt="nav logo" className="nav-logo" width="30"/>DIAlogs</a>
+                    
+                        <ul className="nav nav-pills">
+                            <li><a href='/Dashboard'>Home</a></li>
+                            <li><a href='/Profile'>Profile</a></li>
+                            <li><a href='/AddCase'>Add a Case</a></li>
+                            <li><a href='/About'>About</a></li>
+                        </ul>
+                    </div>
 
-    <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-            <div className="navbar-header shift">
-                <a className="navbar-brand" href="/Dashboard"><img src="https://vectr.com/champageonponce/aI2dP1Qsu.svg?width=273&height=275&select=aI2dP1Qsupage0" alt="nav logo" className="nav-logo" width="30"/>DIAlogs</a>
-            
-                <ul className="nav navbar-nav">
-                    <li><a href='/Dashboard'>Home</a></li>
-                    <li><a href='/Profile'>Profile</a></li>
-                    <li><a href='/AddCase'>Add a Case</a></li>
-                    <li><a href='/About'>About</a></li>
-                </ul>
-            </div>
+                    <ul className="nav nav-pills">
 
-            <ul className="nav navbar-nav navbar-right">
+                        <li><a href='/logout'>Log out</a></li>
+                        <li><a href="/physician">Login</a></li>
 
-                <li><a href='/logout'><span className="glyphicon glyphicon-user"></span>Log out</a></li>
-                <li><a href="/physician"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-
-            </ul>
-            
+                    </ul>
+                    
+                </div>
+            </nav>
         </div>
-    </nav>
+    </header>
 
     export default Nav;

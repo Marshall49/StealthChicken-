05c9f35941fb4e4beda794e1edf8c6357cfefdca
John Barson <jkbarson@gmail.com>
2017-10-18 13:22:04 -0400

    connect comments to dCase

--- models/comment.js
+++ models/comment.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    physicianId: { 
+    physicianId: {
         type: Schema.Types.ObjectId,
         ref: "Physician"
     },
-    dateCreated: { 
-        type: Date, 
-        default: Date.now 
+    dateCreated: {
+        type: Date,
+        default: Date.now
     },
     body: {
         type: String
+    },
+    dCaseId: {
+      type:mongoose.Schema.Types.ObjectId, ref: 'dCase'
     }
 });
 

5101fe837f382b123a9836b116a6c1a394bdfa32
Jess <jsscthomp@gmail.com>
2017-10-19 10:22:00 -0400

    trying to pull

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
+// dependencies
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+// Routes
 const routes = require("./routes/API");
 
-
+// Authorization & security dependencies
 const http = require('https');
 const morgan = require('morgan');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+
 //Mongoose Models
-const Physician = require("./models/physician.js");
+const User = require("./models/physician.js");
 const dCase = require("./models/dCase.js");
-const dexcom = require("./models/dexcom.js");
+const Dexcom = require("./models/dexcom.js");
+const Comment = require('./models/comment');
+
 // Oauth2 library
 const ClientOAuth2 = require('client-oauth2');
 
+// PORT address
 const PORT = process.env.PORT || 3001;
 
-//Here are the Heroku deploy "Mlab" Mongo URI for the Dexcom Client Secret and the mongo lab
-const CLIENT_SECRET = process.env.DEXCOM_CLIENT_SECRET || "";
-const MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://localhost/Stealth_Chicken'
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
 app.use(function(req, res, next) {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
@@ -38,10 +46,10 @@ app.use(function(req, res, next) {
     next();
   }
 });
-// Serve up static assets
-app.use(express.static("client/build"));
-// Add routes, both API and view
-app.use(routes);
+
+//Here are the Heroku deploy "Mlab" Mongo URI for the Dexcom Client Secret and the mongo lab
+const CLIENT_SECRET = process.env.DEXCOM_CLIENT_SECRET || "";
+const MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://localhost/Stealth_Chicken'
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 //Set up default mongoose connection

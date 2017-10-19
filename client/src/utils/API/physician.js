cc7fb7bd7b84a8400298f9170de2c187c93259cb
Jess <jsscthomp@gmail.com>
2017-10-19 11:02:38 -0400

    got server working

--- client/src/utils/API/physician.js
+++ client/src/utils/API/physician.js
@@ -4,15 +4,15 @@ export default {
 //pertaining to PHYSICIAN data
     //Sends user data (Person, email, speciality, member since date, maybe pic) to backend for the LOGIN
 	sendUser: function(userData) {
-		return axios.get("/api/physician", userData);
+		return axios.get("/api/user", userData);
     },
     // Gets user data from db
     getUser: function(id) {
-        return axios.get("/api/physician/" + id);
+        return axios.get("/api/user/" + id);
     },
     //Save user to db
     saveUser: function(userData) {
-        return axios.post("/api/physician", userData);
+        return axios.post("/api/user", userData);
 				// console.log("user posted")
     }
 };
